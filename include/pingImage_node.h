cd19427b34ceb05ca1e1394c011d0341a40e3bcd
Daiki Hamada <63084329+starD-ham@users.noreply.github.com>
2023-02-02 19:21:02 +0900

    save

--- include/pingImage_node.h
+++ include/pingImage_node.h
@@ -6,7 +6,6 @@
 #define PING_CPP_PINGIMAGE_NODE_H
 #include "ros/ros.h"
 #include "Original_msgs/Ping360.h"
-#include "opencv2/opencv.hpp"
 #include "sensor_msgs/Image.h"
 #include "sensor_msgs/LaserScan.h"
 #include "cv_bridge/cv_bridge.h"
@@ -20,20 +19,19 @@ public:
     ros::Subscriber m_echoSub;
     ros::Publisher m_imgPub;
     void echoCb(const Original_msgs::Ping360::ConstPtr& msg);
-    std::string state;
-    ros::Time active_time;
 
     cv_bridge::CvImage bridge;
     cv::Mat mat_image;
-    cv::Mat jet_image;
     void publishImage();
-    const int x_reso=4*2*50;// 4*2*50だと描画に時間がかかる
-    const int y_reso=4*2*50;//
+    const int x_reso=4*2*50;
+    const int y_reso=4*2*50;
     const double pi = 2*acos(0.0);
     float center[2]={float(x_reso/2),float(y_reso/2)};
     std::vector<float> deg;
     std::vector<float> r_dist;
+
     sensor_msgs::Image m_image;
+
 };
 
 
--- src/pingImage_node.cpp
+++ src/pingImage_node.cpp
@@ -7,75 +7,69 @@
 
 PingImage_node::PingImage_node() {
     this->m_echoSub = this->m_nh.subscribe("/echo",1000,&PingImage_node::echoCb, this);
-    this->m_imgPub=this->m_nh.advertise<sensor_msgs::Image>("/image_sonar",1000);
+    this->m_imgPub=this->m_nh.advertise<sensor_msgs::Image>("/image_debug",1000);
     mat_image = cv::Mat::zeros(cv::Size(x_reso,y_reso),CV_8U);
     publishImage();
-    this->state="READY";
-    ROS_INFO("%f",this->pi);
 }
 
 void PingImage_node::echoCb(const Original_msgs::Ping360::ConstPtr &msg) {
-    ROS_INFO("Received echo");
-    //active_time=ros::Time::now();
+    ROS_INFO("callback");
     float linear_factor=msg->intensities.size()/float(center[0]);//中央までのデータ数
     int point_color;// 色の値
+
     for(int i=0;i<int(center[0]);i++){//中央から端まで色塗りをしていく
         if(i<center[0]){
-            point_color=msg->intensities[int(i*linear_factor-1)];//linear_factorに従って色を決める
+            point_color=msg->intensities[int(i*linear_factor-1)];//
         }
         else{
             point_color=0;
         }
         for(int k=0;k<2;k++){//先の4角度分を同じ色として色塗りしておく
-            float theta=2* this->pi*(msg->angle+k)/400.0+pi;
-            float x = float(i)*cos(theta+pi);
+            float theta=2* this->pi*(msg->angle+k)/400.0;
+            float x = float(i)*cos(theta);
             float y = float(i)*sin(theta);
+
             // 描画　描画位置はズレていたため調整
             mat_image.at<cv::Vec2b>(int(center[0]+x),int(center[1]+y/2+center[1]/2))=point_color;
         }
     }
-    if(msg->angle==399){
-        cv::imwrite("/home/hamada/catkin_ws/src/ping360/image/"+std::to_string(ros::Time::now().sec)+".jpg",mat_image);
-    }
     publishImage();
-    state="ACTIVE";
 }
 
 void PingImage_node::publishImage() {
 
+
     // カラーマップの描画
-    cv::applyColorMap(mat_image,jet_image,cv::COLORMAP_JET);//カラーマップ変換
+    cv::Mat jet_img;
+    cv::applyColorMap(mat_image,jet_img,cv::COLORMAP_JET);
+
     // 距離を示す円の描画
-    cv::circle(jet_image,cv::Point(int(center[0]),int(center[1])),x_reso/2,cv::Scalar(255,255,255),1,cv::LINE_8);
-    cv::circle(jet_image,cv::Point(int(center[0]),int(center[1])),x_reso/3,cv::Scalar(255,255,255),1,cv::LINE_8);
-    cv::circle(jet_image,cv::Point(int(center[0]),int(center[1])),x_reso/6,cv::Scalar(255,255,255),1,cv::LINE_8);
-    bridge.image=jet_image;// ImageMSGに変換するための形式に
+    cv::circle(jet_img,cv::Point(int(center[0]),int(center[1])),x_reso/2,cv::Scalar(255,255,255),0,cv::LINE_8);
+    cv::circle(jet_img,cv::Point(int(center[0]),int(center[1])),x_reso/3,cv::Scalar(255,255,255),0,cv::LINE_8);
+    cv::circle(jet_img,cv::Point(int(center[0]),int(center[1])),x_reso/6,cv::Scalar(255,255,255),0,cv::LINE_8);
+    // カメラ撮影範囲の描画
+    float camera_angle=96.1*3/4/180*3.14;//カメラの水中での画角
+    float x_diff=0.1*x_reso/1.5;//カメラとソナーのx位置 TODO:ピクセル数に変換する
+    float y_diff=0;//カメラとソナーのy位置
+    //cv::line(jet_img,cv::Point(int(center[0]-x_diff),int(center[1])),cv::Point(int(center[0]+center[1]*tan(camera_angle)-x_diff),0),cv::Scalar(255,255,255),2,cv::LINE_4);
+    //cv::line(jet_img,cv::Point(int(center[0]-x_diff),int(center[1])),cv::Point(int(center[0]+center[1]*tan(-camera_angle)-x_diff),0),cv::Scalar(255,255,255),2,cv::LINE_4);
+
+    bridge.image=jet_img;
     bridge.encoding="bgr8";
     bridge.header.stamp=ros::Time::now();
-    this->m_imgPub.publish(bridge.toImageMsg());
-    ROS_INFO("published");
-    // 角度を表す線の描画
-//    cv::line(jet_img,cv::Point(int(center[0])*2/3,0),cv::Point(int(center[0])*2/3,int(2*center[1])*2/3),cv::Scalar(255,255,255));
-//    cv::line(jet_img,cv::Point(0,int(center[1])*2/3),cv::Point(int(2*center[0])*2/3,int(center[1])*2/3),cv::Scalar(255,255,255));
-//    cv::line(jet_img,cv::Point(int(center[0])*2/3+int(center[0])*2/3*cos(pi/4),int(center[1])+int(center[1]*2/3)*sin(pi/4)),cv::Point(int(center[0]*2/3)-int(center[0])*2/3*cos(pi/4),int(center[1]*2/3)-int(center[1])*2/3*sin(pi/4)),cv::Scalar(255,255,255));
-//    cv::line(jet_img,cv::Point(int(center[0])*2/3+int(center[0])*2/3*cos(pi/4),int(center[1])-int(center[1]*2/3)*sin(pi/4)),cv::Point(int(center[0]*2/3)-int(center[0])*2/3*cos(pi/4),int(center[1]*2/3)+int(center[1])*2/3*sin(pi/4)),cv::Scalar(255,255,255));
-//    // 角度の文字の描画
 
-    //ガウシアン
-//    cv::Mat gaussian;
-//    cv::GaussianBlur(jet_img, gaussian, cv::Size(9, 9), 5);
+
+    this->m_imgPub.publish(bridge.toImageMsg());
+    ROS_INFO("publish now");
 }
 
 int main(int argc, char **argv){
     ros::init(argc,argv,"pingImage_node");
     PingImage_node pingImageNode;
-    ros::Rate rate(5);
+    ros::Rate rate(20);
     while(ros::ok()){
-        ROS_INFO_THROTTLE(1, "%s", pingImageNode.state.data());
-        if(pingImageNode.state=="ACTIVE"){
-            pingImageNode.state="READY";
-        }
         ros::spinOnce();
         rate.sleep();
     }
+
 }
